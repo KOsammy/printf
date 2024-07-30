4874e68c458c45e2b38a0c413d4943ce07b608c5
Johnkennedy Umeh <126101340+Johnkenzzy@users.noreply.github.com>
2024-07-30 17:47:08 +0100

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -8,51 +8,49 @@
  *
  * Return: the number of characters printed (excludung the null byte)
  */
-int _printf(const char *format, ...)
-{
-	va_list args;
-	int count = 0;
-	const char *p;
+int _printf(const char *format, ...) {
+    va_list args;
+    va_start(args, format);
+    int count = 0;
 
-	va_start(args, format);
-	for (p = format; *p != '\0'; p++)
-	{
-		if (*p == '%')
-		{
-			p++;
-			if (*p == '\0')
-				break;
-			switch (*p)
-			{
-				case 'c':
-					{
-					char c = (char) va_arg(args, int);
-					print_char(c);
-					count++;
-					break;
-					}
-				case 's':
-					{
-					char *s = va_arg(args, char *);
-					print_str(s);
-					count++;
-					break;
-					}
-				default:
-					{
-					putchar('%');
-					putchar(*p);
-					count += 2;
-					break;
-					}
-			}
-		}
-		else
-		{
-			putchar(*p);
-			count++;
-		}
-	}
-	va_end(args);
-	return count;
+    while (*format) {
+        if (*format == '%') {
+            format++;
+            switch (*format) {
+                case 'c': {
+                    char c = (char) va_arg(args, int);
+                    putchar(c);
+                    count++;
+                    break;
+                }
+                case 's': {
+                    char *s = va_arg(args, char *);
+                    while (*s) {
+                        putchar(*s);
+                        s++;
+                        count++;
+                    }
+                    break;
+                }
+                case '%': {
+                    putchar('%');
+                    count++;
+                    break;
+                }
+                default: {
+                    putchar('%');
+                    putchar(*format);
+                    count += 2;
+                    break;
+                }
+            }
+        } else {
+            putchar(*format);
+            count++;
+        }
+        format++;
+    }
+
+    va_end(args);
+    return count;
 }
