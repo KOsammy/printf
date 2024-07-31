943433648e5d4403304db38658d01a3648f7fbd2
johnkenzzy <johnkenumeh@gmail.com>
2024-07-30 21:07:41 -0700

    syntax edits

--- 3-print_funcs.c
+++ 3-print_funcs.c
@@ -4,12 +4,11 @@
 
 /***=====FORMAT SPECIFIERS FUNCTIONS======***/
 
-/**
- * print_unsigned - prints unsigned integer
- * @arg: takes an unsigned integer as argument
- * return: count
- */
-int print_unsigned_int_format(va_list args)
+	/**
+	* print_unsigned - prints unsigned integer
+	* @arg: takes an unsigned integer as argument
+	* return: count
+	*/int print_unsigned_int_format(va_list args)
 {
 	unsigned int num = va_arg(args, unsigned int);
 	int count = 0;
@@ -39,12 +38,11 @@ int print_unsigned_int_format(va_list args)
 }
 
 
-/**
- * print_octal - prints integer in octal format
- * @arg: argument
- * return: count
- */
-int print_octal_format(va_list args)
+	/**
+	* print_octal - prints integer in octal format
+	* @arg: argument
+	* return: count
+	*/int print_octal_format(va_list args)
 {
 	unsigned int num = va_arg(args, unsigned int);
 	int count = 0;
@@ -74,17 +72,18 @@ int print_octal_format(va_list args)
 }
 
 
-/**
- * print_lowerhexadecimal - prints lowercase hexadecimal
- * @n: argument
- * return: nothing
-*/
-int print_hex_format(va_list args)
+	/**
+	* print_lowerhexadecimal - prints lowercase hexadecimal
+	* @n: argument
+	* return: nothing
+	*/int print_hex_format(va_list args)
 {
 	unsigned int num = va_arg(args, unsigned int);
 	int count = 0;
 	char buffer[20];
 	int i = 0;
+	int remainder;
+	int j;
 
 	if (num == 0)
 	{
@@ -94,7 +93,7 @@ int print_hex_format(va_list args)
 
 	while (num > 0)
 	{
-		int remainder = num % 16;
+		remainder = num % 16;
 		if (remainder < 10)
 			buffer[i++] = remainder + '0';
 		else
@@ -102,11 +101,11 @@ int print_hex_format(va_list args)
 		num = num / 16;
 	}
 
-	for (int j = i - 1; j >= 0; j--)
+	for (j = i - 1; j >= 0; j--)
 	{
 		_putchar(buffer[j]);
 		count++;
 	}
 
 	return (count);
-i}
+}
