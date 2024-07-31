63e958e792e4c04f24b670ab05f937e454839e04
Johnkennedy Umeh <126101340+Johnkenzzy@users.noreply.github.com>
2024-07-31 05:32:03 +0100

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -103,8 +103,6 @@ int handle_format(char format, va_list args)
 		count = print_int_format(args);
 	else if (format == 'b')
 		count = print_binary_format(args);
-	else if (format == 'x' || format == 'X')
-		count = print_hex_format(args);
 	else if (format == 'u')
 		count = print_unsigned_int_format(args);
 	else if (format == '%')
