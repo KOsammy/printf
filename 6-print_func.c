d8b9e1b367135bc33885263e504e6dde0bde3ee0
johnkenzzy <johnkenumeh@gmail.com>
2024-07-31 05:32:39 -0700

    created the print address format function

---
+++ 6-print_func.c
@@ -0,0 +1,45 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include "main.h"
+
+
+	/**
+	 * print_pointer_format - prints the address of a pointer
+	 * @args: argument
+	 * return: count
+	 */int print_pointer_format(va_list args)
+{
+	void *ptr = va_arg(args, void *);
+	unsigned long num = (unsigned long)ptr;
+	int count = 0;
+	char buffer[20];
+	int i = 0;
+	int j;
+
+	if (ptr == NULL)
+	{
+		printf("(nil)");
+		return (5);
+	}
+
+	while (num > 0)
+	{
+		int remainder = num % 16;
+		if (remainder < 10)
+			buffer[i++] = remainder + '0';
+		else
+			buffer[i++] = (remainder - 10) + 'a';
+		num = num / 16;
+	}
+
+	printf("0x");
+	count += 2;
+
+	for (j = i - 1; j >= 0; j--)
+	{
+		_putchar(buffer[j]);
+		count++;
+	}
+
+	return (count);
+}
